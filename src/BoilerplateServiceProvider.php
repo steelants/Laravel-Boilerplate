11f61c667a899a1846d8aad5c11f2b7a6c14293c
JonatanRek <gamerclassn7@gmail.com>
2023-10-19 08:53:13 +0200

    Complete initial setup

---
+++ src/BoilerplateServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SteelAnts\LaravelBoilerplate;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModalServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
