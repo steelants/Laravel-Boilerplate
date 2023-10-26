0fb45b2c60d654c0a7eca0836f2b1df9147874fc
JonatanRek <gamerclassn7@gmail.com>
2023-10-26 18:34:50 +0200

    Datatable for audit

---
+++ stubs/app/Http/Livewire/Audit/DataTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Livewire\Audit;
+
+use App\Models\Activity;
+use SteelAnts\DataTable\Http\Livewire\DataTableV2;
+use Illuminate\Database\Eloquent\Builder;
+
+class DataTable extends DataTableV2
+{
+    public $paginated = true;
+    public int $itemsPerPage = 100;
+
+    public function query(): Builder
+    {
+        return Activity::with(["affected", "user"])->orderByDesc("created_at");
+    }
+
+    public function row($row): array
+    {
+        $affectedJson = json_encode([
+            'id'=>$row->affected->id ?? '',
+            'name'=>$row->affected->name ?? '',
+        ], JSON_UNESCAPED_UNICODE);
+
+        return [
+            'created_at' => $row->created_at,
+            'ip_address' => $row->ip,
+            'note' => $row->lang_text ,
+            'user_id' => ($row->user->name ?? 'Unknown'),
+            'affected_id' => $affectedJson,
+        ];
+    }
+
+    public function headers(): array
+    {
+        return ["Created", "IP Address", "Note", "Author", "Model"];
+    }
+}
--- stubs/resources/views/system/audit/index.blade.php
+++ stubs/resources/views/system/audit/index.blade.php
@@ -6,52 +6,6 @@
         <div class="page-header">
             <h1>{{ __('boilerplate::ui.audit') }}</h1>
         </div>
-        <div class="table-responsive">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">{{ __('boilerplate::ui.created') }}</th>
-                        <th scope="col">{{ __('boilerplate::ui.ip_address') }}</th>
-                        <th scope="col">{{ __('boilerplate::ui.note') }}</th>
-                        <th scope="col">{{ __('boilerplate::ui.author') }}</th>
-                        <th scope="col">{{ __('boilerplate::ui.model') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if (!empty($activities) && $activities->count() > 0)
-                        @foreach ($activities as $activity)
-                            <tr>
-                                <th scope="row">{{ $activity->created_at }}</th>
-                                <td>{{ $activity->ip }}</td>
-                                <td>{{ $activity->lang_text }}</td>
-                                <td>{{ !empty($activity->user) ? $activity->user->name : (isset($activity->user_id) && $activity->user_id === 0 ? __('user.Console') : __('user.UserDeleted')) }}</td>
-                                <td>
-                                     @if (isset($activity->affected))
-                                        {{ json_encode([
-                                            'id'=>$activity->affected->id,
-                                            'name'=>$activity->affected->name,
-                                        ], JSON_UNESCAPED_UNICODE) }}
-                                    @else
-                                        @if (isset($urls[$activity->id]))
-                                            @if ($urls[$activity->id] != '')
-                                                <a href="{{ $urls[$activity->id] }}">{{ __('boilerplate::ui.show') }}</a>
-                                            @else
-                                                {{ __('boilerplate::ui.deleted') }}
-                                            @endif
-                                        @else
-                                            {{ __('boilerplate::ui.deleted') }}
-                                        @endif
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td class="text-center" colspan="5">{{ __('boilerplate::ui.not_found') }}</td>
-                        </tr>
-                    @endif
-                </tbody>
-            </table>
-        </div>
+        @livewire('audit.data-table', [], key('data-table'))
     </div>
 @endsection
