567072234de86b9dd02420d9b6ca76587ce024ec
JonatanRek <gamerclassn7@gmail.com>
2023-11-06 08:36:46 +0100

    Move Helpers Registration to service provider

--- src/BoilerplateServiceProvider.php
+++ src/BoilerplateServiceProvider.php
@@ -7,15 +7,35 @@ use Illuminate\Support\ServiceProvider;
 use SteelAnts\LaravelBoilerplate\Console\Commands\InstallCommand;
 use SteelAnts\LaravelBoilerplate\Console\Commands\MakeBasicTestsCommand;
 
+use App\Models\Tenant;
+use App\Services\TenantManager;
+
 class BoilerplateServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'boilerplate');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'boilerplate');
 
         $this->publishes([
-            __DIR__.'/../lang' => $this->app->langPath('vendor/boilerplate'),
+            __DIR__ . '/../lang' => $this->app->langPath('vendor/boilerplate'),
         ]);
+
+
+        if (!function_exists('tenantManager')) {
+            /** @return TenantManager */
+            function tenantManager()
+            {
+                return app(TenantManager::class);
+            }
+        }
+
+        if (!function_exists('tenant')) {
+            /** @return Tenant */
+            function tenant()
+            {
+                return app(TenantManager::class)->getTenant();
+            }
+        }
 
         if (!$this->app->runningInConsole()) {
             return;
