e7f2d231a79dcab618dfaf0b4efb260c2a09549d
JonatanRek <gamerclassn7@gmail.com>
2023-11-15 18:14:37 +0100

    Update navigation.blade.php

--- stubs/resources/views/partials/navigation.blade.php
+++ stubs/resources/views/partials/navigation.blade.php
@@ -13,37 +13,39 @@
             </a>
         </li>
         {{-- MAIN NAVIGATION ALL --}}
-        <li class="mt-4 text-muted"><small>{{ __('boilerplate::ui.system') }}</small></li>
-        <li class="nav-item {{ request()->routeIs('system.audit.index') ? 'is-active' : '' }}">
-            <a class="nav-link" href="{{ route('system.audit.index') }}">
-                <i class="nav-link-ico fas fa-eye"></i>
-                {{ __('boilerplate::ui.audit') }}
-            </a>
-        </li>
-        <li class="nav-item {{ request()->routeIs('system.user.index') ? 'is-active' : '' }}">
-            <a class="nav-link" href="{{ route('system.user.index') }}">
-                <i class="nav-link-ico fas fa-users"></i>
-                {{ __('boilerplate::ui.user') }}
-            </a>
-        </li>
-        <li class="nav-item {{ request()->routeIs('system.subscription.index') ? 'is-active' : '' }}">
-            <a class="nav-link" href="{{ route('system.subscription.index') }}">
-                <i class="nav-link-ico fas fa-dollar-sign"></i>
-                {{ __('boilerplate::subscriptions.title') }}
-            </a>
-        </li>
-        <li class="nav-item {{ request()->routeIs('system.log.index') ? 'is-active' : '' }}">
-            <a class="nav-link" href="{{ route('system.log.index') }}">
-                <i class="nav-link-ico fas fa-bug"></i>
-                {{ __('boilerplate::ui.log') }}
-            </a>
-        </li>
-        <li class="nav-item {{ request()->routeIs('system.jobs.index') ? 'is-active' : '' }}">
-            <a class="nav-link" href="{{ route('system.jobs.index') }}">
-                <i class="nav-link-ico fas fa-business-time"></i>
-                {{ __('boilerplate::ui.jobs') }}
-            </a>
-        </li>
-        {{-- MAIN NAVIGATION SYSTEM --}}
+        @auth
+            <li class="mt-4 text-muted"><small>{{ __('boilerplate::ui.system') }}</small></li>
+            <li class="nav-item {{ request()->routeIs('system.audit.index') ? 'is-active' : '' }}">
+                <a class="nav-link" href="{{ route('system.audit.index') }}">
+                    <i class="nav-link-ico fas fa-eye"></i>
+                    {{ __('boilerplate::ui.audit') }}
+                </a>
+            </li>
+            <li class="nav-item {{ request()->routeIs('system.user.index') ? 'is-active' : '' }}">
+                <a class="nav-link" href="{{ route('system.user.index') }}">
+                    <i class="nav-link-ico fas fa-users"></i>
+                    {{ __('boilerplate::ui.user') }}
+                </a>
+            </li>
+            <li class="nav-item {{ request()->routeIs('system.subscription.index') ? 'is-active' : '' }}">
+                <a class="nav-link" href="{{ route('system.subscription.index') }}">
+                    <i class="nav-link-ico fas fa-dollar-sign"></i>
+                    {{ __('boilerplate::subscriptions.title') }}
+                </a>
+            </li>
+            <li class="nav-item {{ request()->routeIs('system.log.index') ? 'is-active' : '' }}">
+                <a class="nav-link" href="{{ route('system.log.index') }}">
+                    <i class="nav-link-ico fas fa-bug"></i>
+                    {{ __('boilerplate::ui.log') }}
+                </a>
+            </li>
+            <li class="nav-item {{ request()->routeIs('system.jobs.index') ? 'is-active' : '' }}">
+                <a class="nav-link" href="{{ route('system.jobs.index') }}">
+                    <i class="nav-link-ico fas fa-business-time"></i>
+                    {{ __('boilerplate::ui.jobs') }}
+                </a>
+            </li>
+            {{-- MAIN NAVIGATION SYSTEM --}}
+        @endauth
     </ul>
 </div>
