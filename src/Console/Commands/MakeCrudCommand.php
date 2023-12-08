6c92edd80c2b99b96abfa161f0ffbecc526340cb
JonatanRek <gamerclassn7@gmail.com>
2023-12-08 12:28:02 +0100

    Add Datatable Headers

--- src/Console/Commands/MakeCrudCommand.php
+++ src/Console/Commands/MakeCrudCommand.php
@@ -17,7 +17,7 @@ class MakeCrudCommand extends Command
     public function handle(): void
     {
         $model = $this->argument('model');
-        if (!class_exists('App\\Models\\'.$model)) {
+        if (!class_exists('App\\Models\\' . $model)) {
             $this->components->error('Model not Found!');
             return;
         }
@@ -28,59 +28,59 @@ class MakeCrudCommand extends Command
         Artisan::call('livewire:discover');
     }
 
-    private function makeClassFile(string $path,string $fileName, string $model){
-        $testFilePath = base_path() . '/'. $path .'/' . $fileName;
+    private function makeClassFile(string $path, string $fileName, string $model)
+    {
+        $testFilePath = base_path() . '/' . $path . '/' . $fileName;
         if (file_exists($testFilePath)) {
             if (!$this->components->confirm("The [" . $testFilePath . "] test already exists. Do you want to replace it?")) {
                 return;
             }
         }
 
-        $folderpath = str_replace('/'.$fileName, "", $testFilePath);
+        $folderpath = str_replace('/' . $fileName, "", $testFilePath);
         if (!file_exists($folderpath)) {
             mkdir($folderpath, 0777, true);
         }
 
         $this->components->info("creting File" . $testFilePath);
-        if ($fileName == "Form.php"){
+        if ($fileName == "Form.php") {
             Artisan::call('make:livewire ' . $model . '.Form');
-        } elseif($fileName == "DataTable.php") {
+        } elseif ($fileName == "DataTable.php") {
             $content = $this->GetDataTableClassSkeleton([
-                'model' => $model
+                'model' => $model,
+                'headers' => (new ('App\\Models\\' . $model))->getFillable() ,
             ]);
             file_put_contents($testFilePath, $content);
         }
-
-
     }
 
-    private function GetDataTableClassSkeleton(array $arguments){
+    private function GetDataTableClassSkeleton(array $arguments)
+    {
         return '<?php
-namespace App\\Http\\Livewire\\'.$arguments['model'].';
+namespace App\\Http\\Livewire\\' . $arguments['model'] . ';
 
-use App\\Models\\'.$arguments['model'].';
+use App\\Models\\' . $arguments['model'] . ';
 use SteelAnts\\DataTable\Http\\Livewire\DataTableV2;
 use Illuminate\\Database\Eloquent\\Builder;
 
 class DataTable extends DataTableV2
 {
     public $listeners = [
-        \''.strtolower($arguments['model']).'Added\' => \'$refresh\'
+        \'' . strtolower($arguments['model']) . 'Added\' => \'$refresh\'
     ];
 
     public function query(): Builder
     {
-        return '.$arguments['model'].'::query();
+        return ' . $arguments['model'] . '::query();
     }
 
     public function headers(): array
     {
-        return ["id", "actions"];
+        return ["id", "'. implode('","', $arguments['headers']) . '", "actions"];
     }
 
-
-    public function remove($'.strtolower($arguments['model']).'_id){
-        '.$arguments['model'].'::find($'.strtolower($arguments['model']).'_id)->delete();
+    public function remove($' . strtolower($arguments['model']) . '_id){
+        ' . $arguments['model'] . '::find($' . strtolower($arguments['model']) . '_id)->delete();
     }
 }';
     }
