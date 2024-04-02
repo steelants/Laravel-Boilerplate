a6d8d5f9668068d173792ae83cc1f08c07c8a8bb
JonatanRek <gamerclassn7@gmail.com>
2024-04-02 12:18:07 +0200

    Settings Started

---
+++ prefabs/database/migrations/2024_04_02_095547_create_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->nullableMorphs('settable');
+            $table->string('index')->indexed();
+            $table->string('type', 50)->default('string');
+            $table->text('value');
+            $table->timestamps();
+            $table->unique(['settable_type', 'settable_id', 'index']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
