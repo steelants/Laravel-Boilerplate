ae3aede8b083a9b19e31014a2116852cf3f685ca
JonatanRek <gamerclassn7@gmail.com>
2024-04-04 19:24:35 +0200

    Faccade Fixes

--- prefabs/app/Models/Session.php
+++ prefabs/app/Models/Session.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Attribute;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
