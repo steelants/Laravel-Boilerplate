a5044fc87702f64cf3ed102541b126494f108d4a
JonatanRek <gamerclassn7@gmail.com>
2024-04-06 16:49:06 +0200

    Language Fixes

---
+++ lang/en/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'password' => 'Password',
+    'email' => 'Email',
+    'name' => 'Name',
+    'register' => 'Register',
+    'login' => 'login',
+    'password_reset' => 'Password Reset',
+    'password_confirm' => 'Configm Password',
+    'send_password_reset' => 'Send password Reset',
+    'remember' => 'Remmeber me',
+];
