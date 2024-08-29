8ba1844f024ae7e6e951184a6104648c3bca43dc
JonatanRek <gamerclassn7@gmail.com>
2024-08-29 08:02:42 +0200

    Create SizeHelper.php

---
+++ prefabs/app/Helpers/SizeHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helpers;
+
+class SizeHelper
+{
+  public function returnBytes($size_str)
+    {
+        switch (substr($size_str, -1)) {
+            case 'M':
+            case 'm':
+                return (int) $size_str * 1048576;
+            case 'K':
+            case 'k':
+                return (int) $size_str * 1024;
+            case 'G':
+            case 'g':
+                return (int) $size_str * 1073741824;
+            default:
+                return $size_str;
+        }
+    }
+  }
