0a0fd88bde4c0de71157d70737fe4227c4bf8730
Patrik Sztefek <patrik.sztefek@gmail.com>
2024-11-08 13:44:22 +0100

    changelog

---
+++ prefabs/resources/views/changelog/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout-app>
+    <div class="container-xl">
+        <div class="page-header">
+            <h1>{{ __('Changelog') }}</h1>
+        </div>
+
+        {!! $content !!}
+    </div>
+</x-layout-app>
