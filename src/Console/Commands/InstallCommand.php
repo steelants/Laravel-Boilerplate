29ec499009eca902d97575bca040e8ec5e1e78fb
JonatanRek <gamerclassn7@gmail.com>
2024-12-31 19:03:27 +0100

    Fix Stubs

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -26,10 +26,13 @@ class InstallCommand extends Command
 
         if (strpos(file_get_contents($RouteFilePath), 'Route::auth();') === false) {
             //If authentication not installed install
+            $this->call('install:api' . ($this->option('force') ? ' --force' : ''));
             $this->call('install:auth' . ($this->option('force') ? ' --force' : ''));
             //Artisan::call('install:auth --force');
             file_put_contents($RouteFilePath, str_replace('Route::auth();' ,'', file_get_contents($RouteFilePath)));
         }
+
+
 
         $this->components->info('Installing Boilerplate Scaffolding');
 
