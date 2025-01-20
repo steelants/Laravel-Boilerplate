f10275487205b04c49341dc2f15b480090ef6134
JonatanRek <gamerclassn7@gmail.com>
2025-01-20 13:22:49 +0100

    Update MenuItem.php

--- src/Support/MenuItem.php
+++ src/Support/MenuItem.php
@@ -3,17 +3,28 @@
 namespace SteelAnts\LaravelBoilerplate\Support;
 
 use Exception;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 class MenuItem
 {
+    protected Collection $subMenus;
+
     public function __construct(public string $title, public string $id, public string $route, public string $icon, public array $parameters = [])
     {
         if (!Route::has($route)) {
             throw new Exception("route with name: " . $route . " dont exists!", 1);
         }
     }
+
+    public function add(string $title, array $options = [])
+	{
+		$item = new MenuItem($title, ...$options);
+		$this->subMenus->push($item);
+
+		return $this;
+	}
 
     public function isUse(): bool
     {
