d7e80350d6abaee6b98ecf11b7b76037b6092922
Patrik Sztefek <patrik.sztefek@gmail.com>
2025-03-09 17:15:18 +0100

    fix quill livewire syncing

--- prefabs/resources/js/boilerplate/quill.js
+++ prefabs/resources/js/boilerplate/quill.js
@@ -25,87 +25,98 @@ Quill.register(Quill.import('attributors/style/direction'), true);
 Quill.register(Quill.import('attributors/style/font'), true);
 Quill.register(Quill.import('attributors/style/size'), true);
 
-window.loadQuill = function(){
-    document.querySelectorAll('.quill-editor:not(.ready)').forEach(function(element){
-        let container = element.closest('.quill-container');
-        let textarea = container.querySelector('.quill-textarea');
+window.loadQuill = function(element, $wire = null){
+    let container = element.closest('.quill-container');
+    let textarea = container.querySelector('.quill-textarea');
 
-        const toolbarOptions = [
-            [{ header: [1, 2, 3, false] }],
-            ['bold', 'italic', 'underline', 'strike'],
-            ['blockquote', 'code-block'],
-            ['link', 'image'],
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-            ['table'],
-            ['clean'],
-        ];
+    const toolbarOptions = [
+        [{ header: [1, 2, 3, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        ['blockquote', 'code-block'],
+        ['link', 'image'],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+        ['table'],
+        ['clean'],
+    ];
 
-        const atValues = [
-            { id: 1, value: "Fredrik Sundqvist" },
-            { id: 2, value: "Patrik Sjölin" }
-          ];
+    const atValues = [
+        { id: 1, value: "Fredrik Sundqvist" },
+        { id: 2, value: "Patrik Sjölin" }
+      ];
 
-        let quill = new Quill(element, {
-            theme: 'snow',
-            modules: {
-                table: true,
-                tableUI: true,
-                toolbar: {
-                    container: toolbarOptions,
-                    handlers: {
-                        table: function() {
-                            this.quill.getModule('table').insertTable(3, 3);
-                        }
-                    }
-                },
-                clipboard: {
-                    matchVisual: false
-                },
-                mention: {
-                    allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
-                    mentionDenotationChars: ["@", "#"],
-                    source: function(searchTerm, renderList, mentionChar) {
-                      let values;
-
-                      if (mentionChar === "@") {
-                        values = atValues;
-                      } else {
-                        values = hashValues;
-                      }
-
-                      if (searchTerm.length === 0) {
-                        renderList(values, searchTerm);
-                      } else {
-                        const matches = [];
-                        for (let i = 0; i < values.length; i++)
-                          if (
-                            ~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase())
-                          )
-                            matches.push(values[i]);
-                        renderList(matches, searchTerm);
-                      }
+    let quill = new Quill(element, {
+        theme: 'snow',
+        modules: {
+            table: true,
+            tableUI: true,
+            toolbar: {
+                container: toolbarOptions,
+                handlers: {
+                    table: function() {
+                        this.quill.getModule('table').insertTable(3, 3);
                     }
                 }
+            },
+            clipboard: {
+                matchVisual: false
+            },
+            mention: {
+                allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+                mentionDenotationChars: ["@", "#"],
+                source: function(searchTerm, renderList, mentionChar) {
+                  let values;
+
+                  if (mentionChar === "@") {
+                    values = atValues;
+                  } else {
+                    values = hashValues;
+                  }
+
+                  if (searchTerm.length === 0) {
+                    renderList(values, searchTerm);
+                  } else {
+                    const matches = [];
+                    for (let i = 0; i < values.length; i++)
+                      if (
+                        ~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase())
+                      )
+                        matches.push(values[i]);
+                    renderList(matches, searchTerm);
+                  }
+                }
             }
-        });
-
-        // let table = quill.getModule('table')
-        // container.querySelector('.ql-insert-table').addEventListener('click', function(){
-        //     console.log('click');
-        //     table.insertTable(2, 2);
-        // });
-
-        quill.root.innerHTML = textarea.value;
-
-        quill.on('text-change', function () {
-            let value = quill.root.innerHTML;
-            textarea.value = value;
-            textarea.dispatchEvent(new Event('input'));
-            console.log(quill.getContents());
-        });
-
-        element.classList.add('ready');
-        container.querySelector('.quill-loading').remove();
+        }
     });
+
+    // let table = quill.getModule('table')
+    // container.querySelector('.ql-insert-table').addEventListener('click', function(){
+    //     console.log('click');
+    //     table.insertTable(2, 2);
+    // });
+
+    quill.root.innerHTML = textarea.value;
+
+    quill.on('text-change', function (delta, oldDelta, source) {
+        let value = quill.root.innerHTML;
+        textarea.value = value;
+        textarea.dispatchEvent(new Event('input'));
+    });
+
+    textarea.addEventListener('change', function(){
+        quill.root.innerHTML = textarea.value;
+    });
+
+    if($wire){
+        $wire.hook('morphed', function(){
+            quill.root.innerHTML = textarea.value;
+        });
+    }
+
+    element.classList.add('ready');
+    container.querySelector('.quill-loading').remove();
 }
-window.loadQuill();
+
+document.querySelectorAll('.quill-editor:not(.quill-livewire):not(.ready)').forEach(function(element){
+    window.loadQuill(element);
+});
+
