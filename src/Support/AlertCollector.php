b0341adbc763942f925ccd48ea676e1e268df0d3
Haitem <silentislp@gmail.com>
2025-05-15 15:18:04 +0200

    Change session facade use

--- src/Support/AlertCollector.php
+++ src/Support/AlertCollector.php
@@ -2,24 +2,24 @@
 
 namespace SteelAnts\LaravelBoilerplate\Support;
 
-use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Session;
 use SteelAnts\LaravelBoilerplate\Types\AlertModeType;
 
 class AlertCollector
 {
 	public function add(string $type = 'info', string $text, string $icon = '', int $mode = AlertModeType::RELOAD)
 	{
-		$alerts = session('alerts-' . $mode, []);
+		$alerts = Session::get('alerts-' . $mode, []);
 		$alerts[] = new AlertItem(type: $type, text: $text, icon: $icon);
 		if ($mode == AlertModeType::RELOAD) {
-			session()->flash('alerts-'. $mode, $alerts);
+			Session::flash('alerts-'. $mode, $alerts);
 		} else if ($mode == AlertModeType::INSTANT) {
-			session()->now('alerts-'. $mode, $alerts);
+			Session::now('alerts-'. $mode, $alerts);
 		}
 	}
 
 	public function get(int $mode = AlertModeType::RELOAD)
 	{
-		return session('alerts-' . $mode, []);
+		return Session::get('alerts-' . $mode, []);
 	}
 }
