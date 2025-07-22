4b2373fdf752d3b97f11b1f7e0a3e702046dcc37
JonatanRek <gamerclassn7@gmail.com>
2025-07-22 07:43:02 +0200

    Config

--- prefabs/config/boilerplate.php
+++ prefabs/config/boilerplate.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    /*
+	/*
     |--------------------------------------------------------------------------
     | System Admins
     |--------------------------------------------------------------------------
@@ -9,5 +9,11 @@ return [
     |
     */
 
-    'system_admins' => explode(',', env('APP_SYSTEM_ADMINS', '')),    
+	'system_admins' => explode(',', env('APP_SYSTEM_ADMINS', '')),
+	'system_admins_mail' => explode(',', env('APP_SYSTEM_ADMINS', '')),
+	'backups' => [
+		'database' =>  (bool) env('BACKUP_DATABASE', true),
+		'storage' =>  (bool) env('BACKUP_STORAGE', true),
+		'enviroment' =>  (bool) env('BACKUP_ENV', true),
+	],
 ];
