f1d2c5d0adf62917b95c6f3a4bf5766491ef0278
Imendin <SilentisLP@gmail.com>
2025-08-04 18:44:00 +0200

    #LB-67 - Migrace v boileprlate má v sobě chybu
    Complete

---
+++ src/Types/FileType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SteelAnts\LaravelBoilerplate\Types;
+
+class FileType
+{
+    const INLINE = 0;
+    const ATTACHMENT = 1;
+
+    public static function getNames()
+    {
+        return $names = [
+            self::INLINE     => 'Text',
+            self::ATTACHMENT => 'Attachment',
+        ];
+    }
+
+    public static function getName($type)
+    {
+        $names = self::getNames();
+        if (isset($names[$type])) {
+            return __($names[$type]);
+        }
+        return 'NULL';
+    }
+
+    public static function getkeys()
+    {
+        return array_keys(self::getNames());
+    }
+}
