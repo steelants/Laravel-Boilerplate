9dd42fb9acb62ed4f4d7905589f0f3bb163bc9ad
Patrik Sztefek <patrik.sztefek@gmail.com>
2025-08-09 18:54:08 +0200

    selectbox fix

--- resources/views/components/selectbox.blade.php
+++ resources/views/components/selectbox.blade.php
@@ -1,4 +1,4 @@
-<div class="dropdown {{ $groupClass }}"
+<div class="dropdown selectbox {{ $groupClass }}"
     x-data="initSelectbox($wire, @js($property ?? null), @js($options), @js($multiple), @js($pills), null, @js($required))"
 >
     @isset($label)
@@ -60,24 +60,26 @@
         @if($searchable)
             <input class="dropdown-input" type="text" placeholder="{{ __('Search') }}..." x-model="search">
         @endif
-        <template x-for="(option, index) in filteredOptions" :key="option.id">
-            @isset($optionTemplate)
-                {{ $optionTemplate }}
-            @else
-                <div class="dropdown-item"
-                    :tabindex="index"
-                    @click="selectOption(option)"
-                    @keyup.enter="selectOption(option)"
-                >
-                    <input class="form-check-input pe-none" tabindex="-1" type="checkbox" id="{{$id}}" :checked="isSelectedOption(option)"/>
+		<div class="dropdown-items">
+			<template x-for="(option, index) in filteredOptions" :key="option.id">
+				@isset($optionTemplate)
+					{{ $optionTemplate }}
+				@else
+					<div class="dropdown-item"
+						:tabindex="index"
+						@click="selectOption(option)"
+						@keyup.enter="selectOption(option)"
+					>
+						<input class="form-check-input pe-none" tabindex="-1" type="checkbox" id="{{$id}}" :checked="isSelectedOption(option)"/>
 
-                    @isset($customOption)
-                        {{ $customOption }}
-                    @else
-                        <span x-text="option.name"></span>
-                    @endif
-                </div>
-            @endisset
-        </template>
+						@isset($customOption)
+							{{ $customOption }}
+						@else
+							<span x-text="option.name"></span>
+						@endif
+					</div>
+				@endisset
+			</template>
+		</div>
     </div>
 </div>
