54281ec6b2bf465a593fa5685df537f58b55c7ac
Jonatan Rek <gamerclassn7@gmail.com>
2025-09-06 23:54:04 +0200

    Fix Crud

--- prefabs/app/Http/Controllers/System/UserController.php
+++ prefabs/app/Http/Controllers/System/UserController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\System;
 
 use App\Http\Controllers\BaseController;
 use SteelAnts\LaravelBoilerplate\Traits\CRUD;
+use App\Models\User;
 
 class UserController extends BaseController
 {
     use CRUD;
-    public string $model = "user";
+    public string $model = User::class;
 }
