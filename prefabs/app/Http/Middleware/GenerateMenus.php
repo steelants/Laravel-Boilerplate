2b45c1c4a0342da6cb7465b2db47c3a535e16c41
Jonatan Rek <gamerclassn7@gmail.com>
2025-09-06 23:51:24 +0200

    Optimize menu generation

--- prefabs/app/Http/Middleware/GenerateMenus.php
+++ prefabs/app/Http/Middleware/GenerateMenus.php
@@ -11,91 +11,77 @@ use Symfony\Component\HttpFoundation\Response;
 class GenerateMenus
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+    * Handle an incoming request.
+    *
+    * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+    */
     public function handle(Request $request, Closure $next): Response
     {
         if (Auth::guest()) {
             return $next($request);
         }
-
+        
         if ($request->route()->getName() === 'livewire.message') {
             return $next($request);
         }
-
-        Menu::make('main-menu', function ($menu) {
-            $systemRoutes = [
-                'boilerplate::ui.home' => [
-                    ' fas fa-home',
-                    'home',
-                ],
-            ];
-            foreach ($systemRoutes as $title => $route_data) {
+        
+        $menuRoutes = [
+            'boilerplate::ui.home' => [
+                ' fas fa-home',
+                'home',
+            ],
+        ];
+        
+        $systemRoutes = [
+            'boilerplate::ui.audit'            => [
+                'fas fa-eye',
+                'system.audit.index',
+            ],
+            'boilerplate::ui.user'             => [
+                'fas fa-users',
+                'system.user.index',
+            ],
+            'boilerplate::subscriptions.title' => [
+                'fas fa-dollar-sign',
+                'system.subscription.index',
+            ],
+            'boilerplate::ui.log'              => [
+                'fas fa-bug',
+                'system.log.index',
+            ],
+            'boilerplate::ui.jobs'             => [
+                'fas fa-business-time',
+                'system.jobs.index',
+            ],
+            'boilerplate::ui.cache'            => [
+                'fas fa-box',
+                'system.cache.index',
+            ],
+            'boilerplate::ui.backup'           => [
+                'fas fa-file-archive',
+                'system.backup.index',
+            ],
+        ];
+        
+        $menus = [
+            'main-menu' => $menuRoutes,
+            'system-menu' => $systemRoutes,
+        ];
+        
+        foreach ($menus as $menuKey => $MenuItems) {
+            $menu = Menu::get($menuKey) ?? Menu::make($menuKey, function () {});
+            foreach ($MenuItems as $title => $route_data) {
                 $icon = $route_data[0];
                 $route = $route_data[1];
-
+                
                 $menu->add($title, [
                     'id'    => strtolower($title),
                     'icon'  => $icon,
                     'route' => $route,
                 ]);
             }
-        });
-
-        //CHECK IF USER IS SYSTEM ADMIN
-        Menu::make('system-menu', function ($menu) {
-            $systemRoutes = [
-                'boilerplate::ui.audit'            => [
-                    'fas fa-eye',
-                    'system.audit.index',
-                ],
-                'boilerplate::ui.user'             => [
-                    'fas fa-users',
-                    'system.user.index',
-                ],
-                'boilerplate::subscriptions.title' => [
-                    'fas fa-dollar-sign',
-                    'system.subscription.index',
-                ],
-                'boilerplate::ui.log'              => [
-                    'fas fa-bug',
-                    'system.log.index',
-                ],
-                'boilerplate::ui.jobs'             => [
-                    'fas fa-business-time',
-                    'system.jobs.index',
-                ],
-                'boilerplate::ui.cache'            => [
-                    'fas fa-box',
-                    'system.cache.index',
-                ],
-                'boilerplate::ui.backup'           => [
-                    'fas fa-file-archive',
-                    'system.backup.index',
-                ],
-            ];
-
-            if (file_exists(base_path() . '/routes/api.php')) {
-                $systemRoutes['boilerplate::ui.api'] = [
-                    'fas fa-rocket',
-                    'system.api.index',
-                ];
-            }
-
-            foreach ($systemRoutes as $title => $route_data) {
-                $icon = $route_data[0];
-                $route = $route_data[1];
-
-                $menu->add($title, [
-                    'id'    => strtolower($title),
-                    'icon'  => $icon,
-                    'route' => $route,
-                ]);
-            }
-        });
-
+        }
+        
         return $next($request);
     }
 }
