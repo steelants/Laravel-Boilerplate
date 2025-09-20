16e08f1f537b223c4ce717881ab46f6eb84afde7
Jonatan Rek <gamerclassn7@gmail.com>
2025-09-20 12:44:24 +0200

    Fix Files

--- database/migrations/2024_10_28_140259_alter_user_id_to_activities_table.php
+++ database/migrations/2024_10_28_140259_alter_user_id_to_activities_table.php
@@ -13,31 +13,8 @@ return new class extends Migration
 	 */
 	public function up(): void
 	{
-		if (!Schema::hasColumn('activities', 'actor_id')) {
-			Schema::table('activities', function (Blueprint $table) {
-				$table->nullableMorphs(name: 'actor');
-			});
-		}
-
-		Activity::chunk(500, function ($activities) {
-			if (!empty($activities) && $activities->count() > 0) {
-				foreach ($activities as $activity) {
-					if (empty($activity->user_id)) {
-						continue;
-					}
-					$user = User::find($activity->user_id);
-					if (empty($user) || $user->count() <= 0) {
-						continue;
-					}
-					$activity->actor()->associate($user);
-					$activity->save();
-				}
-			}
-		});
-
-		Schema::table('activities', function (Blueprint $table) {
-			$table->dropForeign(['user_id']);
-			$table->dropColumn('user_id');
+		Schema::table('files', function (Blueprint $table) {
+			$table->morphs('fileable')->nullbale();
 		});
 	}
 
@@ -45,26 +22,5 @@ return new class extends Migration
 	 * Reverse the migrations.
 	 */
 	public function down(): void
-	{
-		Schema::table('activities', function (Blueprint $table) {
-			$table->foreignId("user_id")->nullable();
-			$table->foreign("user_id")->references("id")->on("users")->constrained();
-		});
-
-		$activities = Activity::all();
-		if (!empty($activities) && $activities->count() > 0) {
-			foreach ($activities as $activity) {
-				$actor = $activity->actor?->getModel();
-				if (empty($actor) || $actor->count() <= 0 || get_class($actor) != User::class) {
-					continue;
-				}
-				$activity->user_id = $actor->id;
-				$activity->save();
-			}
-		}
-
-		Schema::table('activities', function (Blueprint $table) {
-			$table->dropMorphs('actor');
-		});
-	}
+	{ }
 };
