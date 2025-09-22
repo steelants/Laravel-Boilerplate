c962cae05546c53481651f9d1fefb172bb6fcadf
Jonatan Rek <gamerclassn7@gmail.com>
2025-09-23 00:34:41 +0200

    fix

--- prefabs/app/Http/Controllers/System/SubscriptionController.php
+++ prefabs/app/Http/Controllers/System/SubscriptionController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\System;
 use App\Http\Controllers\BaseController;
 use SteelAnts\LaravelBoilerplate\Models\Subscription;
 use SteelAnts\LaravelBoilerplate\Traits\CRUD;
-use SteelAnts\LaravelBoilerplate\Traits\HasSystemLayout;
+use SteelAnts\LaravelBoilerplate\Traits\SystemPage;
 
 class SubscriptionController extends BaseController
 {
-	use CRUD, HasSystemLayout;
+	use CRUD, SystemPage;
 	public string $model = Subscription::class;
 
 	public function __construct() {
