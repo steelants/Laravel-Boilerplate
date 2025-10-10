331f9f78c218caacc48514f161132bbddaad101d
Patrik Sztefek <patrik.sztefek@gmail.com>
2025-10-10 20:53:39 +0200

    system file layout

--- prefabs/resources/views/system/file/index.blade.php
+++ prefabs/resources/views/system/file/index.blade.php
@@ -1,8 +1,8 @@
-<x-layout-app>
+<x-dynamic-component :component="$layout">
     <div class="container-xl">
         <div class="page-header">
             <h1>{{ __('boilerplate::ui.file') }}</h1>
         </div>
         @livewire('boilerplate.file.gallery', [], key('data-table'))
     </div>
-</x-layout-app>
+</x-dynamic-component>
