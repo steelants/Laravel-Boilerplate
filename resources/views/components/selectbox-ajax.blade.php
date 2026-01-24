e3116292ca243e327a7259b8ab5ba6590ffb511c
Patrik Sztefek <patrik.sztefek@gmail.com>
2026-01-24 18:03:39 +0100

    sorry

--- resources/views/components/selectbox-ajax.blade.php
+++ resources/views/components/selectbox-ajax.blade.php
@@ -1,7 +1,7 @@
 <div class="dropdown selectbox {{ $groupClass }}"
     x-data="initSelectboxAjax($wire, @js($property ?? null), @js($options), @js($multiple), @js($pills), null, @js($required))"
 	wire:key="{{ md5(json_encode($options)) }}"
->hello
+>
     @isset($label)
         <label class="form-label">{{ $label }}</label>
     @endisset
