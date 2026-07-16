adc060e72e42640f740aae97b1a365247aba8c9f
Haitem <silentislp@gmail.com>
2026-07-16 15:40:00 +0200

    Add safeguard function

--- prefabs/app/Providers/AppServiceProvider.php
+++ prefabs/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use URL;
 
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+		DB::prohibitDestructiveCommands(app()->isProduction());
+
         if (config('app.env') != 'local') {
             URL::forceScheme('https');
         }
